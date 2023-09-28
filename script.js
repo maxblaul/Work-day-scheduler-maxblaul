a9dffa185c5d45448bf302f2d0affbbd3512ec44
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-09-28 00:45:52 -0500

    made more changes to homework

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var $textarea = document.querySelector("#col-8 col-md-10 description");
+var $saveButton = document.querySelector("#btn saveBtn col-2 col-md-1");
 var currentDay = dayjs();
-$('#1a').text(currentDay.format('MMM D, YYYY'))
+$('#1a').text(currentDay.format('MMM D, YYYY'));
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,6 +13,20 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  // $button.addEventListener("click", function(event) {
+  //   event.preventDefault();
+  } );
+    $saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var calendarContents = {
+      text: $textarea.value
+    };
+
+    localStorage.setItem("calendarContents", JSON.stringify
+    (calendarContents));
+    renderMessage();
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,6 +36,10 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  // function _handleButtonClick(){
+    // localStorage.setItem("textarea", $textarea.val())
+  }
+
+  
   // TODO: Add code to display the current date in the header of the page.
-});
+);
